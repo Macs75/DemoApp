b391d784e9339e1c1532e9378e6696b0ca2f1cba
Macs75 <macs75@gmail.com>
2015-02-16 01:06:15 +0100

    Gognalys verification

--- app/src/main/java/com/sixdegrees/demoapp/activities/MainActivity.java
+++ app/src/main/java/com/sixdegrees/demoapp/activities/MainActivity.java
@@ -1,58 +1,68 @@
 package com.sixdegrees.demoapp.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.Contacts;
-import android.provider.ContactsContract;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.ContactsContract;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
+import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.android.contacts.common.util.ViewUtil;
 import com.sixdegrees.demoapp.R;
 import com.sixdegrees.demoapp.util.PrefUtils;
-import com.thrivecom.ringcaptcha.RingcaptchaApplication;
-import com.thrivecom.ringcaptcha.RingcaptchaApplicationHandler;
-import com.thrivecom.ringcaptcha.RingcaptchaVerification;
+
+import com.matesnetwork.callverification.Cognalys;
+import com.matesnetwork.interfaces.VerificationListner;
+
+import java.security.MessageDigest;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String API_KEY = "lufo4ofa3umy6o2e9ojo";
-    private static final String API_SECRET = "4icu2e5o7ojo2ibepi4y";
-
+    //private static final String API_KEY = "lufo4ofa3umy6o2e9ojo";
+    //private static final String API_SECRET = "4icu2e5o7ojo2ibepi4y";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.i(TAG, "SHA : " + getSHA(this));
         PrefUtils.init(this);
 
         // Check if the EULA has been accepted; if not, show it.
         if (!PrefUtils.isTosAccepted(this)) {
             Intent intent = new Intent(this, WelcomeActivity.class);
             startActivity(intent);
-            finish();
         }
 
         if (PrefUtils.isPhoneVerified(this).equals("")){
-            RingcaptchaApplication.verifyPhoneNumber(getApplicationContext(), API_KEY, API_SECRET, new RingcaptchaApplicationHandler() {
+            Intent intent = new Intent(this, VerifyNumberActivity.class);
+            startActivity(intent);
+             /*RingcaptchaApplication.verifyPhoneNumber(getApplicationContext(), API_KEY, API_SECRET, new RingcaptchaApplicationHandler() {
                 private static final String TAG = "Ringcaptcha verification";
                 @Override
                 public void onSuccess(RingcaptchaVerification rcObj) {
@@ -67,8 +77,7 @@ public class MainActivity extends ActionBarActivity {
                     Log.i(TAG, "cancel");
                     finish();
                 }
-            });
-            finish();
+            });*/
         }
 
         setContentView(R.layout.activity_main);
@@ -81,8 +90,33 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    public String getSHA(Context context) {
+        String sign=null;
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                sign = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+            }
+        } catch (Exception e) {
+        }
+        return sign;
+    }
 
+    @Override
+    protected void onRestart (){
+        super.onRestart();
+        if ( !PrefUtils.isTosAccepted(this)  ){
+            Log.i(TAG, "Legal disclaimer not accepted!");
+            finish();
+        }
+        if ( PrefUtils.isPhoneVerified(this).equals("")  ){
+            Log.i(TAG, "Phone number not verified!");
+            finish();
+        }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/sixdegrees/demoapp/activities/VerifyNumberActivity.java
@@ -0,0 +1,127 @@
+package com.sixdegrees.demoapp.activities;
+
+import android.app.Activity;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.matesnetwork.callverification.Cognalys;
+import com.sixdegrees.demoapp.R;
+import com.matesnetwork.interfaces.VerificationListner;
+import com.sixdegrees.demoapp.util.PrefUtils;
+
+/**
+ * Created by Macs on 15/02/2015.
+ */
+public class VerifyNumberActivity extends Activity {
+    private TextView timertv = null;
+    private EditText phoneNumbTv = null;
+    private CountDownTimer countDownTimer;
+    private RelativeLayout timerLayout;
+    private static String APP_ID="8468ffa2bf7e4604984392d";
+    private static String ACCESS_TOKEN="e8a124d73ce441b0bec0bce11a083b3086ef0122";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_verify_number);
+        phoneNumbTv = (EditText) findViewById(R.id.ph_et);
+        timertv = (TextView) findViewById(R.id.timer_tv);
+        TextView country_code_tv = (TextView) findViewById(R.id.country_code_tv);
+        country_code_tv.setText(Cognalys.getCountryCode(getApplicationContext()));
+        timerLayout = (RelativeLayout) findViewById(R.id.timer_rl);
+
+        findViewById(R.id.verifybutton).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        if (!TextUtils.isEmpty(phoneNumbTv.getText().toString())) {
+                            verify();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Please enter your phone number to verify", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void verify() {
+        timerLayout.setVisibility(View.VISIBLE);
+        countDownTimer = new CountDownTimer(60000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timertv.setText("" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timerLayout.setVisibility(View.GONE);
+            }
+
+        };
+        countDownTimer.start();
+
+        Cognalys.verifyMobileNumber(VerifyNumberActivity.this, ACCESS_TOKEN, APP_ID, phoneNumbTv.getText().toString(), new VerificationListner() {
+
+                    @Override
+                    public void onVerificationStarted() {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onVerificationSuccess() {
+                        countDownTimer.cancel();
+                        timerLayout.setVisibility(View.GONE);
+                        PrefUtils.setPhoneVerified(VerifyNumberActivity.this, phoneNumbTv.getText().toString());
+                        showAlert("Your number has been verified\n\nThanks!!",true);
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(ArrayList<String> errorList) {
+                        countDownTimer.cancel();
+                        timerLayout.setVisibility(View.GONE);
+                        for (String error : errorList) {
+                            Log.e("abx", "error:"+error);
+                        }
+                        showAlert("Something went wrong.\n please try again",false);
+                    }
+                });
+
+
+    }
+    private void showAlert(String message,boolean status){
+        final Dialog dialog = new Dialog(VerifyNumberActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog);
+
+
+        ImageView mImageView = (ImageView) dialog.findViewById(R.id.verify_im);
+        TextView messageTv=(TextView) dialog.findViewById(R.id.messagetv);
+        if (status) {
+            mImageView.setImageResource(R.drawable.blue_tick);
+        }else{
+            mImageView.setImageResource(R.drawable.wrong);
+        }
+
+        messageTv.setText(message);
+        dialog.show();
+    }
+
+}
